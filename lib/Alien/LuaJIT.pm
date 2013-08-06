70be49a52d26e3e92eb4a9b6d24b988ba42a8711
Steffen Mueller <smueller@cpan.org>
2013-08-06 07:52:49 +0200

    Initial version of Alien::LuaJIT

---
+++ lib/Alien/LuaJIT.pm
@@ -0,0 +1,39 @@
+package Alien::LuaJIT;
+use 5.14.0;
+use warnings;
+
+our $VERSION = '0.01';
+use parent 'Alien::Base';
+
+1;
+__END__
+
+=head1 NAME
+
+Alien::LuaJIT - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Alien::LuaJIT;
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+L<http://www.luajit.org>
+
+L<Alien::Base>
+
+=head1 AUTHOR
+
+Steffen Mueller, E<lt>smueller@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2013 by Steffen Mueller
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
